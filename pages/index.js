0546f3541d5123b0302941d9365a3f9dcea76718
oguzhan.cevik <oguzhan@90pixel.com>
2022-12-11 12:21:11 +0300

    home page edit

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,73 @@
-import { useRef } from "react";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
-import GithubIcon from "@mui/icons-material/GitHub";
-import styles from "../styles/Home.module.css";
+import { useRef } from 'react';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import GithubIcon from '@mui/icons-material/GitHub';
+import styles from '../styles/Home.module.css';
 import Button from '@mui/material/Button';
 import SwipeDownRoundedIcon from '@mui/icons-material/SwipeDownRounded';
 
-
-
-
 function Home() {
-   const skillRef = useRef();
+  const skillRef = useRef();
 
-   const handleClick=()=>{
-     skillRef.current.scrollIntoViewIfNeeded({ behavior: 'smooth' }) 
-   }
+  const handleClick = () => {
+    skillRef.current.scrollIntoViewIfNeeded({ behavior: 'smooth' });
+  };
   return (
     <div className={styles.home}>
       <div className={styles.about}>
         <h2> Hi, My Name is Oğuzhan...</h2>
         <div className={styles.prompt}>
-          <p className={styles.detail}>A Full-Stack developer, deploying web applications from scratch, and selecting the technologies most suitable for each project.</p>
-          
-          <a target="_newblank" href="https://www.linkedin.com/in/oguzhancevikk/"><LinkedInIcon /></a> 
-          <a target="_newblank" href="mailto: oguzhan728@gmail.com"><EmailIcon /></a>
-         <a target="_newblank" href="https://github.com/worm-codes"><GithubIcon /></a> 
+          <p className={styles.detail}>
+            A Full-Stack developer, deploying web applications from scratch, and
+            selecting the technologies most suitable for each project.
+          </p>
+
+          <a
+            target="_newblank"
+            href="https://www.linkedin.com/in/oguzhancevikk/"
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            target="_newblank"
+            href="mailto: oguzhan728@gmail.com"
+          >
+            <EmailIcon />
+          </a>
+          <a
+            target="_newblank"
+            href="https://github.com/worm-codes"
+          >
+            <GithubIcon />
+          </a>
         </div>
-       <Button size="large" onClick={handleClick} className={styles.skill_button} variant="contained">SKILLS<SwipeDownRoundedIcon/></Button>
-      
+        <Button
+          size="large"
+          onClick={handleClick}
+          className={styles.skill_button}
+          variant="contained"
+        >
+          SKILLS
+          <SwipeDownRoundedIcon />
+        </Button>
       </div>
-      <div  className={styles.skills}>
-        <h1 > Skills</h1>
-        <ol  className={styles.list}>
+      <div className={styles.skills}>
+        <h1> Skills</h1>
+        <ol className={styles.list}>
           <li className={styles.item}>
-            <h2 > Front-End</h2>
+            <h2> Front-End</h2>
             <span>
-               ReactJS, ContextAPI, HTML5, CSS3, NPM,
-               BootStrap, MaterialUI, NextJS(Learning), TypeScript(Learning)
+              ReactJS, ContextAPI, HTML5, CSS3, Styled Components, Motion
+              Library, Redux Toolkit, React Query, SCSS, NPM, BootStrap,
+              MaterialUI, NextJS, TypeScript(Learning)
             </span>
           </li>
-          
+
           <li className={styles.item}>
             <h2 ref={skillRef}>Back-End</h2>
-            <span>
-              NodeJS, ExpressJS, MongoDB, Socket.IO
-            </span>
+            <span>NodeJS, ExpressJS, MongoDB, Socket.IO</span>
           </li>
-          <li  className={styles.item}>
+          <li className={styles.item}>
             <h2>Languages</h2>
             <span>JavaScript, Java, Python, C#, C</span>
           </li>
