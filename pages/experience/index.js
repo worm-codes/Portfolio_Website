ba8c1a48adf38914a3ff61aa755c34ae9e2932ab
oguzhan.cevik <oguzhan@90pixel.com>
2023-01-20 10:17:56 +0300

    reorder experience page

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -15,32 +15,6 @@ const Experience = () => {
         <VerticalTimeline lineColor="black">
           <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="2017 Sep - 2018 Jan"
-            iconStyle={{ background: 'black', color: '#fff' }}
-            icon={<SchoolIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              <a
-                target="_newblank"
-                href="https://en.mcbu.edu.tr/"
-              >
-                Manisa Celal Bayar University
-              </a>{' '}
-              , Manisa/Turkey
-            </h3>
-
-            <h4 className="vertical-timeline-element-subtitle">
-              English Preparation School
-            </h4>
-            <p>
-              My department was %100 English so I had to take this class that
-              lasts 2 terms. I passed the exam in January(first term) and I took
-              a break from school to find what I want.
-            </p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--education"
             date="2018 Sep - 2022 Jul"
             iconStyle={{ background: 'black', color: '#fff' }}
             icon={<SchoolIcon />}
@@ -92,8 +66,34 @@ const Experience = () => {
             </div>
           </VerticalTimelineElement>
           <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2017 Sep - 2018 Jan"
+            iconStyle={{ background: 'black', color: '#fff' }}
+            icon={<SchoolIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              <a
+                target="_newblank"
+                href="https://en.mcbu.edu.tr/"
+              >
+                Manisa Celal Bayar University
+              </a>{' '}
+              , Manisa/Turkey
+            </h3>
+
+            <h4 className="vertical-timeline-element-subtitle">
+              English Preparation School
+            </h4>
+            <p>
+              My department was %100 English so I had to take this class that
+              lasts 2 terms. I passed the exam in January(first term) and I took
+              a break from school to find what I want.
+            </p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2022 Oct - "
+            date="2022 Oct - Present "
             iconStyle={{ background: '#e9d35b', color: '#fff' }}
             icon={<WorkIcon />}
           >
@@ -262,25 +262,108 @@ const Experience = () => {
               </a>
             </p>
           </VerticalTimelineElement>
-
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 May - 2020 Sep"
+            date="2021 Oct - 2022 Jan"
             iconStyle={{ background: 'green', color: '#fff' }}
             icon={<AssignmentTurnedInIcon />}
           >
             <h3 className="vertical-timeline-element-title">
-              Web Developer Bootcamp, Udemy
+              Full Stack Developer Bootcamp
             </h3>
 
             <p>
-              It was my first course about web development, so It is valuable to
-              me :)
+              Full Stack Developer Bootcamp(144 hours) is about React.js,
+              Node.js and Django and organized by Patika and Alotech. I was also
+              chosen 40 out of 800 people for this bootcamp.{' '}
             </p>
+
             <p>
               <a
                 target="_newblank"
-                href="https://www.udemy.com/certificate/UC-67a2daa7-fc25-4afb-adf8-1ca093e9012d/"
+                href="https://verified.cv/en/verify/20667764076707"
+              >
+                Certificate &#8594;
+              </a>
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2021 Oct"
+            iconStyle={{ background: 'green', color: '#fff' }}
+            icon={<AssignmentTurnedInIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Cybersecurity Awareness Trainig
+            </h3>
+
+            <p>
+              In this time of high cybercrime, we must be careful about links,
+              devices and environment. Amazon shared the cyber security
+              awareness videos that prepared for amazon employees to the public.
+              And, gave a certificate who completes all stages. I completed all
+              stages and got my certificate.{' '}
+            </p>
+
+            <p>
+              <a
+                target="_newblank"
+                href="https://drive.google.com/file/d/1m8Yt3sIo5ZDynWDl55lr_XQKI4GWhGxe/view"
+              >
+                Certificate &#8594;
+              </a>
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2021 Oct"
+            iconStyle={{ background: 'green', color: '#fff' }}
+            icon={<AssignmentTurnedInIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Türk Telekom Cloud Computing Certificate
+            </h3>
+
+            <p>
+              {' '}
+              I have successfully completed the Turk Telekom Cloud Computing
+              Camp, where I am one of the 50 selected among 4412 candidates. In
+              this camp, which lasted for 1 week and lasted for about 20 hours,
+              we gained a lot of information about Cloud Computing and listened
+              to experiences from experts in the sector.
+            </p>
+
+            <p>
+              <a
+                target="_newblank"
+                href="https://drive.google.com/file/d/1nMazl6I6GvIGfnX5sycpmNEhUaqlpyIt/view"
+              >
+                Certificate &#8594;
+              </a>
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2020 Nov - 2021 Feb"
+            iconStyle={{ background: 'green', color: '#fff' }}
+            icon={<AssignmentTurnedInIcon />}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Web Development Career Path
+            </h3>
+
+            <p>
+              I got an free account on Codecademy for free because of Covid
+              Campaign. I tried to finish all content in 2 Month but I could.
+              But, the Campaign was going on so I opened new account and
+              continued to content. After all, I finished and got my certificate
+              in my new account.{' '}
+            </p>
+
+            <p>
+              <a
+                target="_newblank"
+                href="https://www.codecademy.com/profiles/oguzhancevik4056010494/certificates/5b32457b646caa5007c30975"
               >
                 Certificate &#8594;
               </a>
@@ -313,109 +396,22 @@ const Experience = () => {
 
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 Nov - 2021 Feb"
+            date="2020 May - 2020 Sep"
             iconStyle={{ background: 'green', color: '#fff' }}
             icon={<AssignmentTurnedInIcon />}
           >
             <h3 className="vertical-timeline-element-title">
-              Web Development Career Path
+              Web Developer Bootcamp, Udemy
             </h3>
 
             <p>
-              I got an free account on Codecademy for free because of Covid
-              Campaign. I tried to finish all content in 2 Month but I could.
-              But, the Campaign was going on so I opened new account and
-              continued to content. After all, I finished and got my certificate
-              in my new account.{' '}
+              It was my first course about web development, so It is valuable to
+              me :)
             </p>
-
             <p>
               <a
                 target="_newblank"
-                href="https://www.codecademy.com/profiles/oguzhancevik4056010494/certificates/5b32457b646caa5007c30975"
-              >
-                Certificate &#8594;
-              </a>
-            </p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 Oct"
-            iconStyle={{ background: 'green', color: '#fff' }}
-            icon={<AssignmentTurnedInIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Türk Telekom Cloud Computing Certificate
-            </h3>
-
-            <p>
-              {' '}
-              I have successfully completed the Turk Telekom Cloud Computing
-              Camp, where I am one of the 50 selected among 4412 candidates. In
-              this camp, which lasted for 1 week and lasted for about 20 hours,
-              we gained a lot of information about Cloud Computing and listened
-              to experiences from experts in the sector.
-            </p>
-
-            <p>
-              <a
-                target="_newblank"
-                href="https://drive.google.com/file/d/1nMazl6I6GvIGfnX5sycpmNEhUaqlpyIt/view"
-              >
-                Certificate &#8594;
-              </a>
-            </p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 Oct"
-            iconStyle={{ background: 'green', color: '#fff' }}
-            icon={<AssignmentTurnedInIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Cybersecurity Awareness Trainig
-            </h3>
-
-            <p>
-              In this time of high cybercrime, we must be careful about links,
-              devices and environment. Amazon shared the cyber security
-              awareness videos that prepared for amazon employees to the public.
-              And, gave a certificate who completes all stages. I completed all
-              stages and got my certificate.{' '}
-            </p>
-
-            <p>
-              <a
-                target="_newblank"
-                href="https://drive.google.com/file/d/1m8Yt3sIo5ZDynWDl55lr_XQKI4GWhGxe/view"
-              >
-                Certificate &#8594;
-              </a>
-            </p>
-          </VerticalTimelineElement>
-
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 Oct - 2022 Jan"
-            iconStyle={{ background: 'green', color: '#fff' }}
-            icon={<AssignmentTurnedInIcon />}
-          >
-            <h3 className="vertical-timeline-element-title">
-              Full Stack Developer Bootcamp
-            </h3>
-
-            <p>
-              Full Stack Developer Bootcamp(144 hours) is about React.js,
-              Node.js and Django and organized by Patika and Alotech. I was also
-              chosen 40 out of 800 people for this bootcamp.{' '}
-            </p>
-
-            <p>
-              <a
-                target="_newblank"
-                href="https://verified.cv/en/verify/20667764076707"
+                href="https://www.udemy.com/certificate/UC-67a2daa7-fc25-4afb-adf8-1ca093e9012d/"
               >
                 Certificate &#8594;
               </a>
